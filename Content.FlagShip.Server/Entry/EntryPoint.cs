602f767450c913df301992882c38e49a70a1fe7f
Murphy Ibbs <Murphyibbs@gmail.com>
2026-06-29 00:38:02 +0100

    fix

--- Content.FlagShip.Server/Entry/EntryPoint.cs
+++ Content.FlagShip.Server/Entry/EntryPoint.cs
@@ -2,4 +2,13 @@ using Robust.Shared.ContentPack;
 
 namespace Content.FlagShip.Server.Entry;
 
-public sealed class EntryPoint : GameServer;
+public sealed class EntryPoint : GameServer
+{
+    public override void Init()
+    {
+        base.Init();
+
+        Dependencies.BuildGraph();
+        Dependencies.InjectDependencies(this);
+    }
+}
